49681e236c177fa8e7aae5d7671b18b2d5445332
qtlCompleo <quentin.thibaut@compleo.fr>
2025-07-09 15:55:12 +0200

    change version

--- dist/n8n-chat-widget.js
+++ dist/n8n-chat-widget.js
@@ -56,8 +56,8 @@ class N8NChat extends HTMLElement {
 
   async loadTemplate() {
     const [html, css] = await Promise.all([
-      fetch("https://cdn.jsdelivr.net/gh/COMPLEOAGENCY/n8n-chat@v1.0.7/n8n.html").then(res => res.text()),
-      fetch("https://cdn.jsdelivr.net/gh/COMPLEOAGENCY/n8n-chat@v1.0.7/dist/n8n.css").then(res => res.text())
+      fetch("https://cdn.jsdelivr.net/gh/COMPLEOAGENCY/n8n-chat@v1.0.9/n8n.html").then(res => res.text()),
+      fetch("https://cdn.jsdelivr.net/gh/COMPLEOAGENCY/n8n-chat@v1.0.9/dist/n8n.css").then(res => res.text())
     ]);
 
     const template = document.createElement("template");
@@ -135,7 +135,7 @@ class N8NChat extends HTMLElement {
   playNotificationSoundOnce() {
     if (!this.notificationSound) {
       this.notificationSound = new Audio(
-        "https://cdn.jsdelivr.net/gh/COMPLEOAGENCY/n8n-chat@v1.0.7/dist/audio/notification.mp3"
+        "https://cdn.jsdelivr.net/gh/COMPLEOAGENCY/n8n-chat@v1.0.9/dist/audio/notification.mp3"
       );
 
       this.notificationSound
